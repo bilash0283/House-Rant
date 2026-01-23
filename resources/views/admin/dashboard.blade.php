8e90f41fed940e20a3fc5310ed100987c63ac54c
bilash0283 <bilash0283@gmail.com>
2026-01-24 02:10:58 +0600

    dashboard page modify

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,8 +3,6 @@
 @section('title', 'Dashboard')
 
 @section('content')
-    <h1 class="text-2xl font-bold mb-4">Welcome to Dashboard</h1>
-
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-8">
         <div class="bg-white p-6 rounded-xl border shadow-sm">
             <h3 class="text-sm text-slate-500">Total Collected</h3>
