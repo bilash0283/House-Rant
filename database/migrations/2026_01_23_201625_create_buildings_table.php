595338e05f8ac2e74ecd40ff8800382d319fa62f
bilash0283 <bilash0283@gmail.com>
2026-01-24 02:38:54 +0600

    building migration modify

--- database/migrations/2026_01_23_201625_create_buildings_table.php
+++ database/migrations/2026_01_23_201625_create_buildings_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('phone')->nullable();
-            $table->email('email')->unique();
+            $table->string('email')->unique();
             $table->string('address')->nullable();
             $table->timestamps();
         });
